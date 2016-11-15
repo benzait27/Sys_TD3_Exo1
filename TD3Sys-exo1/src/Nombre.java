19f8076d87c1491af7346be67e9cc02dd2d2f710
bz_benzait@esi.dz <Zaki@BENZAIT1993>
2016-11-15 11:57:32 +0100

    exo 1 td3

---
+++ TD3Sys-exo1/src/Nombre.java
@@ -0,0 +1,40 @@
+
+
+public class Nombre  {
+ int n;
+ int carre;
+ int indicateur;
+ 
+ public  Nombre() {
+	super();
+	this.n = 0;
+	this.carre = 0;
+	indicateur=1;
+}
+
+public synchronized void afficher() throws InterruptedException
+ {
+	if (indicateur==1)
+	{
+	indicateur=0;
+	System.out.println("le nombre"+n); 
+	System.out.println("son carre"+carre);
+	notifyAll();
+	}
+	else { wait();}
+ }
+ 
+ public synchronized void calculer() throws InterruptedException
+ {
+	 if (indicateur==0)
+	{
+	indicateur=1;
+	this.n++;
+	this.carre=this.n*this.n;
+	notifyAll();
+	}
+	else { wait();}
+ }
+
+
+}
---
+++ TD3Sys-exo1/src/Threade.java
@@ -0,0 +1,49 @@
+
+public class Threade extends Thread{
+
+	private Nombre nb;
+	private int numF;
+	
+	public Threade(Nombre nb, int numF) {
+		super();
+		this.nb = nb;
+		this.numF = numF;
+	}
+
+	@Override
+	public void run() {
+		
+		if (numF==1) {
+			
+			while (!this.isInterrupted()) {
+				try {
+					sleep(100);
+					nb.afficher();
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+					break;
+				}
+			
+		}} 
+		
+		else   {
+			if (nb.indicateur==0)
+			{
+			try {
+			while (!this.isInterrupted()) {
+			
+				sleep(200);
+				nb.calculer();
+				notifyAll();
+			} }
+			catch (Exception e) {
+				e.printStackTrace();
+				/* on met pas le break car le try est on dehor de while*/
+			}
+		
+	} 
+			}}
+	}
+
+
